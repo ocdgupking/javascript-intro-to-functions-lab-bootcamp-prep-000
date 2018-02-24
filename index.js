f3f690d27010a3bbbbc1b5a4f2abe1da1d4ea14e
guptaco@ymail.com <guptaco@ymail.com>
2018-02-24 23:00:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if string === string.toUpperCase {
     return "YES INDEED!"}
-  else if string === strin..toLowerCase{
+  else if string === string.toLowerCase{
     return "I can't hear you!"}
 }
